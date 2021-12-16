2f68cea43bc997eac09be8c8cab565f126e27849
Dharun Kumar M <66691305+dharunkumar-m@users.noreply.github.com>
2021-12-16 13:13:23 +0530

    Added Security type log generation.

--- toJSON.py
+++ toJSON.py
@@ -1,3 +1,5 @@
+# The Below Code will generate 5 GB of Data
+
 import json
 
 from faker import Faker
@@ -70,6 +72,16 @@ windows_source_provider = DynamicProvider(
      
 fake.add_provider(windows_source_provider)
 
+windows_type_provider = DynamicProvider(
+     provider_name="windows_type",
+     elements=["Application", "Security"] )
+     
+fake.add_provider(windows_type_provider)
+
+security_msg = "An account was successfully logged on.\r\n\r\nSubject:\r\n\tSecurity ID:\t\tS-1-5-18\r\n\tAccount Name:\t\tMS-23$\r\n\tAccount Domain:\t\tWORKGROUP\r\n\tLogon ID:\t\t0x3E7\r\n\r\nLogon Information:\r\n\tLogon Type:\t\t5\r\n\tRestricted Admin Mode:\t-\r\n\tVirtual Account:\t\tNo\r\n\tElevated Token:\t\tYes\r\n\r\nImpersonation Level:\t\tImpersonation\r\n\r\nNew Logon:\r\n\tSecurity ID:\t\tS-1-5-18\r\n\tAccount Name:\t\tSYSTEM\r\n\tAccount Domain:\t\tNT AUTHORITY\r\n\tLogon ID:\t\t0x3E7\r\n\tLinked Logon ID:\t\t0x0\r\n\tNetwork Account Name:\t-\r\n\tNetwork Account Domain:\t-\r\n\tLogon GUID:\t\t{00000000-0000-0000-0000-000000000000}\r\n\r\nProcess Information:\r\n\tProcess ID:\t\t0x36c\r\n\tProcess Name:\t\tC:\\Windows\\System32\\services.exe\r\n\r\nNetwork Information:\r\n\tWorkstation Name:\t-\r\n\tSource Network Address:\t-\r\n\tSource Port:\t\t-\r\n\r\nDetailed Authentication Information:\r\n\tLogon Process:\t\tAdvapi  \r\n\tAuthentication Package:\tNegotiate\r\n\tTransited Services:\t-\r\n\tPackage Name (NTLM only):\t-\r\n\tKey Length:\t\t0\r\n\r\nThis event is generated when a logon session is created. It is generated on the computer that was accessed.\r\n\r\nThe subject fields indicate the account on the local system which requested the logon. This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.\r\n\r\nThe logon type field indicates the kind of logon that occurred. The most common types are 2 (interactive) and 3 (network).\r\n\r\nThe New Logon fields indicate the account for whom the new logon was created, i.e. the account that was logged on.\r\n\r\nThe network fields indicate where a remote logon request originated. Workstation name is not always available and may be left blank in some cases.\r\n\r\nThe impersonation level field indicates the extent to which a process in the logon session can impersonate.\r\n\r\nThe authentication information fields provide detailed information about this specific logon request.\r\n\t- Logon GUID is a unique identifier that can be used to correlate this event with a KDC event.\r\n\t- Transited services indicate which intermediate services have participated in this logon request.\r\n\t- Package name indicates which sub-protocol was used among the NTLM protocols.\r\n\t- Key length indicates the length of the generated session key. This will be 0 if no session key was requested."
+
+#--------------------------------------------------------------------------------
+
 def generateWindowsdata():
     record = {}
   
@@ -77,28 +89,33 @@ def generateWindowsdata():
     record["TIME"] = int(fake.date_time_this_year().timestamp())
     record["SOURCEPORT"] = randint(1, 65535)
     record["SUBHOSTTYPE"] = "WKS"
-    record["SOURCE"] = fake.windows_source()
-    record["TYPE"] = "Application"
+    record["TYPE"] = fake.windows_type()
     
-    if record["SOURCE"] == "nview":
-        record["MSGFIELD"] = "NvAPI function failed with: 'NVAPI_OUT_OF_MEMORY'"
-    elif record["SOURCE"] == "gupdate":
+    if record["TYPE"] == "Security":
+      record["MSGFIELD"] = security_msg
+      record["SOURCE"] = "Microsoft-Windows-Security-Auditing"
+      record["RISK_LEVEL"] = "Low"
+      
+    else:
+      record["SOURCE"] = fake.windows_source()
+      if record["SOURCE"] == "nview":
+        record["MSGFIELD"] = "NvAPI function failed with 'NVAPI_OUT_OF_MEMORY'"
+      elif record["SOURCE"] == "gupdate":
         record["MSGFIELD"] = "Service started "
-    elif record["SOURCE"] == "igccservice":
+      elif record["SOURCE"] == "igccservice":
         record["MSGFIELD"] = "PowerEvent handled successfully by the service."
         
-    
     record["SEVERITY"] = randint(1, 10)
     
     return record
 
 #--------------------------------------------------------------------------------
 
-for i in range(10):
+for i in range(18000000):
     record = generateCiscodata()
     print(json.dumps(record, ensure_ascii=False))
     
-for i in range(10):
+for i in range(18000000):
     record = generateWindowsdata()
     print(json.dumps(record, ensure_ascii=False))
 
